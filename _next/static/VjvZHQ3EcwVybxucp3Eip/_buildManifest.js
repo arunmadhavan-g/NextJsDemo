5d8245af71a0fd311555fc63dd5411848f48ac88
arunmadhavan-g <arunmadhavan.g@gmail.com>
2020-07-04 23:53:00 +0530

    Clears the ghpages to get a fresh build

--- _next/static/VjvZHQ3EcwVybxucp3Eip/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/ProductDetails":["static\u002Fchunks\u002F29107295.edd0a685bd78c8e6e6b6.js","static\u002Fchunks\u002F1bfc9850.e36d87b98b8dfc452ba0.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
